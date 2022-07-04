6d0791a819c3609053e247e5c5e6c9545f3a2818
Jon-Martin Storm <jonmartin.storm@gmail.com>
2022-07-04 12:01:02 +0200

    database testing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
 
-use zero2prod2022::startup::run;
+use zero2prod2022::{startup::run, configuration::{get_configuration}};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
